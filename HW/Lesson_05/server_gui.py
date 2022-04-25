0ad3c803075de91e158fecc3314d1d9a3bb2e251
yura <yuverch@gmail.com>
2022-04-25 21:10:34 +0700

    HW 05 fin +

--- HW/Lesson_05/server_gui.py
+++ HW/Lesson_05/server_gui.py
@@ -84,7 +84,7 @@ class MainWindow(QMainWindow):
         # Настройки геометрии основного окна
         # Поскольку работать с динамическими размерами мы не умеем, и мало времени на изучение, размер окна фиксирован.
         self.setFixedSize(800, 600)
-        self.setWindowTitle('Messaging Server alpha release')
+        self.setWindowTitle('Messaging Server v 0.0.2')
 
         # Надпись о том, что ниже список подключённых клиентов
         self.label = QLabel('Список подключённых клиентов:', self)
