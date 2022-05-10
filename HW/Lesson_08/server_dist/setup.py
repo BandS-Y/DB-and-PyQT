71e7cda4ca740b19a79831121b93defa76be30f0
yura <yuverch@gmail.com>
2022-05-10 14:29:18 +0700

    HW 08 ++

--- HW/Lesson_08/server_dist/setup.py
+++ HW/Lesson_08/server_dist/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="yu_messenger_server",
-      version="0.0.2",
+      version="0.0.3",
       description="Mess Server",
       author="Yuriy Verchenko",
       author_email="yuverch@yandex.ru",
